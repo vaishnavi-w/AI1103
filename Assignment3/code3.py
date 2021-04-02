0955cbf38c21c2fea252c71bbc8faf0133eda274
vaishnavi-w <80573434+vaishnavi-w@users.noreply.github.com>
2021-04-02 10:27:04 +0530

    Update code3.py

--- Assignment3/code3.py
+++ Assignment3/code3.py
@@ -26,10 +26,31 @@ print("Area under pdf of Y_1: " +str(np.trapz(pdf1,x)))
 print("Area under pdf of Y_2: " +str(np.trapz(pdf2,x)))
 print("Area under convoluted pdf: " +str(np.trapz(conv_pdf,x)))
 
+# Region 1 of PDF
+x_1 = np.arange(-4, 1, 0.1)
+y_1 = x_1*0
+# Region 2 of PDF
+x_2 =   np.arange(1, 2, 0.1)
+y_2=  x_2 - 1
+# Region 3 of PDF
+x_3 =   np.arange(2, 3, 0.1)
+y_3=  3-x_3
+# Region 4 of PDF
+x_4 =   np.arange(3, 4, 0.1)
+y_4=  x_4*0
+
+
+# Plotting the points
+
+
 #plotting pdfs of uniform distributions and convoluted function
+plt.plot(x_1, y_1,'b',marker='o',label='X-Theory')
+plt.plot(x_2, y_2,'b',marker='o')
+plt.plot(x_3, y_3,'b',marker='o')
+plt.plot(x_4, y_4,'b',marker='o')
 plt.plot(x,pdf1, label='$Y_2$')
 plt.plot(x,pdf2, label='$Y_1$')
-plt.plot(x,conv_pdf, label='X')
+plt.plot(x,conv_pdf,'r',label='X-Simulation')
 plt.legend(loc='best')
 plt.suptitle('PDFs')
 plt.grid()
@@ -40,9 +61,28 @@ Z=conv_pdf/ (dx * conv_pdf).sum()
 
 # Compute the CDF
 cdf = np.cumsum(Z * dx)
-plt.plot(x,cdf)
+
+#Region 1 of CDF
+x_1 = np.arange(-4, 1, 0.1)
+y_1 = x_1*0
+# Region 2 of CDF
+x_2 =   np.arange(1, 2, 0.1)
+y_2=  (x_2**2-2*x_2+1)/2
+# Region 3 of CDF
+x_3 =   np.arange(2, 3, 0.1)
+y_3=  (6*x_3-x_3**2-7)/2
+# Region 4 of CDF
+x_4 =   np.arange(3, 4, 0.1)
+y_4=  x_4**0
+
+plt.plot(x_1, y_1,'b',marker='o',label='Theory')
+plt.plot(x_2, y_2,'b',marker='o')
+plt.plot(x_3, y_3,'b',marker='o')
+plt.plot(x_4, y_4,'b',marker='o')
+plt.plot(x,cdf,'r',label='Simulation')
 plt.xlabel('Random Variable , X')
 plt.ylabel('$F_{X}(x)$')
 plt.suptitle('CDF of X')
+plt.legend(loc='best')
 plt.grid()
 plt.show()
